18d4a5df70176f3d489f6dd9c307b37a348f78d0
Sofia <sofiaagoncalves25@gmail.com>
2023-12-27 10:34:26 +0000

    Geographical coordinates

--- main.cpp
+++ main.cpp
@@ -3,11 +3,11 @@
 #include "Graph.h"
 #include "HashTable.h"
 #include "Statistics.h"
+#include "Filters.h"
 
 int main() {
     Graph* graph = new Graph();
     HashTable* hashTable = new HashTable();
     Script::infoCollection(graph, hashTable);
-
     return 0;
 }
